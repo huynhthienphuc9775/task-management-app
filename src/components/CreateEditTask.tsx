8a61405521225539f594bb50cda2972b55d9ea76
YaphetS <phucht@brickmate.vn>
2024-12-05 11:46:31 +0700

    fix css

--- src/components/CreateEditTask.tsx
+++ src/components/CreateEditTask.tsx
@@ -57,7 +57,7 @@ const CreateEditTask: FC<Props> = ({ isOpen, onClose }) => {
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <DialogPanel
                             transition
-                            className="sm:w-[700px] w-[90vw] relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                            className="sm:w-[700px] w-[90vw] relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
                         >
                             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 ">
                                 <div className="sm:flex sm:items-start">
